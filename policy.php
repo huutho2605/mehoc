3e0612eb5111ae5e72a07ed32a3735f648229128
huutho2605 <87959772+huutho2605@users.noreply.github.com>
2021-07-30 14:32:29 +0700

    Them chinh sach quyen rieng ty

---
+++ policy.php
@@ -0,0 +1,8 @@
+<h1>Giới thiệu</h1>
+Trang web tại địa chỉ https://mehoc.site/ (Trang web) được điều hành bởi cá nhân Nguyễn Hữu Thọ, có trụ sở kinh doanh chính tại 
+Huyện Cai Lậy, Tỉnh Tiền Giang. Một số công ty con và/hoặc nhãn hiệu của chúng tôi có thể có điều khoản sử dụng 
+và/hoặc chính sách quyền riêng tư của riêng công ty hoặc nhãn hiệu đó, có thể khác nhau, được đăng trên 
+trang web của riêng công ty hoặc nhãn hiệu đó. Chúng tôi khuyến khích bạn xem xét điều khoản sử dụng và/hoặc 
+các chính sách quyền riêng tư đó khi liên kết tới những trang web đó. Nếu không có quy định khác ở các trang web đó, 
+những Điều khoản sử dụng này (bao gồm Chính sách Quyền riêng tư của chúng tôi. Sau đây sẽ áp dụng quy định tương tự) sẽ 
+áp dụng đối với việc sử dụng Trang web của bạn.
