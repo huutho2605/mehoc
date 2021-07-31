41fbadcb0bccfbdfd3eee996bcfa02f970db38fe
huutho2605 <87959772+huutho2605@users.noreply.github.com>
2021-08-01 00:12:15 +0700

    00:12:00 1/08/2021

--- panel/add_quiz.php
+++ panel/add_quiz.php
@@ -42,7 +42,7 @@ if(isset($_POST['add_qs'])){
     mysqli_close($conn);
 };
 ?>
-<button onclick="window.location.href='edit_quiz.php'">CHỈNH SỬA CÂU HỎI</button>
+<!-- <button onclick="window.location.href='edit_quiz.php'">CHỈNH SỬA CÂU HỎI</button> -->
 <form action="add_quiz.php" method="POST">
 <div class="form-group">
     <label>CÂU HỎI:</label> <br>
