128d25c5f555eac1aaaca892f38ebdd2dd2af52c
huutho2605 <87959772+huutho2605@users.noreply.github.com>
2021-07-31 09:54:35 +0700

    Add file sitemap_post/file/quiz

--- config.php
+++ config.php
@@ -1,4 +1,5 @@
 <?php
 $conn = mysqli_connect("103.97.125.243", "mehocsit_root", "mehoc@2020", "mehocsit_mehoc");
 $conn->set_charset("utf8");
+$domain = $_SERVER['SERVER_NAME'];
 ?>
---
+++ sitemap_quiz.php
@@ -0,0 +1,21 @@
+<?php 
+include 'config.php';
+$sql = "SELECT id FROM quiz";
+$result = mysqli_query($conn, $sql);
+$base_url = 'http://'.$domain.'/quiz/?id=';
+header("Content-Type: application/xml; charset=utf-8");
+echo '<?xml version="1.0" encoding="UTF-8"?>'.PHP_EOL; 
+echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">' . PHP_EOL;
+while($row = mysqli_fetch_array($result)){
+    echo '<url>' . PHP_EOL;
+    echo '<loc>'.$base_url. $row["id"] .'/</loc>' . PHP_EOL;
+    echo '<changefreq>always</changefreq>' . PHP_EOL;
+    echo '</url>' . PHP_EOL;
+        // $file = 'sitemap.xml';
+    // Open the file to get existing content
+        // $current = file_get_contents($file);
+    // Append a new person to the file
+}
+echo '</urlset>' . PHP_EOL;
+?>
+
